8db7a58d561895ad3ce703166c8719bd4ce740e8
apentyugov <pentyugov@icloud.com>
2022-07-15 21:55:33 +0400

    add Widgets-MyProductivity component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import {HomeComponent} from './home/home.component';
 import {BreadcrumbComponent} from './layout/main/breadcrumb/breadcrumb.component';
 import {AuthInterceptor} from "./interceptor/auth.interceptor";
 import { NgxMaskModule, IConfig } from 'ngx-mask'
-import {NgxPermissionsModule} from "ngx-permissions";
+import {NgxPermissionsModule, NgxPermissionsRestrictStubModule} from "ngx-permissions";
 import {InfiniteScrollModule} from "ngx-infinite-scroll";
 import {CommonModule} from "@angular/common";
 import { ResetPasswordComponent } from './authentication/reset-password/reset-password.component';
@@ -34,6 +34,10 @@ import {DragDropModule} from "@angular/cdk/drag-drop";
 import {ApplicationsModule} from "./modules/applications/applications.module";
 import {CalendarModule, DateAdapter} from "angular-calendar";
 import {adapterFactory} from "angular-calendar/date-adapters/date-fns";
+import {UcWidgetModule} from "ngx-uploadcare-widget";
+import {MatInputModule} from "@angular/material/input";
+import {NgMultiSelectDropDownModule} from "ng-multiselect-dropdown";
+import {NgxMatDatetimePickerModule, NgxMatNativeDateModule} from "@angular-material-components/datetime-picker";
 
 export function HttpLoaderFactory(http: HttpClient): any {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -60,21 +64,26 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) | null = null;
     ResetPasswordComponent,
   ],
   imports: [
-    NgxMaskModule.forRoot(),
-    CommonModule,
     FormsModule,
+    MaterialModule,
+    PerfectScrollbarModule,
+    FlexLayoutModule,
+    UcWidgetModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    NgxMaskModule.forRoot(),
+    NgxPermissionsRestrictStubModule,
+    InfiniteScrollModule,
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
     SharedModule,
-    PerfectScrollbarModule,
     SimpleNotificationsModule.forRoot(),
     NgxPermissionsModule.forRoot(),
-    InfiniteScrollModule,
     RouterModule.forRoot(AppRoutes, {relativeLinkResolution: 'legacy'}),
     TranslateModule.forRoot({
       loader: {
