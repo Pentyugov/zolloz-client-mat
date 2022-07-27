d53f6a325988c54f125fc8bfbdc0afa2fefc17ad
apentyugov <pentyugov@icloud.com>
2022-07-27 17:54:14 +0400

    Refactor position endpoints

--- src/app/service/position.service.ts
+++ src/app/service/position.service.ts
@@ -16,23 +16,23 @@ export class PositionService implements EntityService<Position> {
   }
 
   public getAll(): Observable<Position[]> {
-    return this.httpClient.get<Position[]>(`${this.host}/position/get-all-positions`);
+    return this.httpClient.get<Position[]>(`${this.host}/positions/`);
   }
 
   public getById(id: string): Observable<Position> {
-    return this.httpClient.get<Position>(`${this.host}/position/get-position-by-id/${id}`);
+    return this.httpClient.get<Position>(`${this.host}/positions/${id}`);
   }
 
   public add(position: Position): Observable<Position> {
-    return this.httpClient.post<Position>(`${this.host}/position/add-new-position`, position);
+    return this.httpClient.post<Position>(`${this.host}/positions/`, position);
   }
 
   public update(position: Position): Observable<Position> {
-    return this.httpClient.put<Position>(`${this.host}/position/update-position`, position);
+    return this.httpClient.put<Position>(`${this.host}/positions/`, position);
   }
 
   public delete(id: string): Observable<CustomHttpResponse> {
-    return this.httpClient.delete<CustomHttpResponse>(`${this.host}/position/delete-position/${id}`);
+    return this.httpClient.delete<CustomHttpResponse>(`${this.host}/positions/${id}`);
   }
 
 
